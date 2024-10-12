b96534491eef389900653dc958ed754bb4035270
Daniel Tan <dtch1997@users.noreply.github.com>
2024-10-12 16:18:01 +0100

    fix

--- config/train_gelu-2l-split_shakespeare-char.py
+++ config/train_gelu-2l-split_shakespeare-char.py
@@ -23,8 +23,8 @@ dropout = 0.2
 per_layer_logit_coefficient = [0, 0, 1]
 
 # Training details
-max_iters = 2000
-lr_decay_iters = 2000 # make equal to max_iters usually
+max_iters = 5000
+lr_decay_iters = 5000 # make equal to max_iters usually
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
